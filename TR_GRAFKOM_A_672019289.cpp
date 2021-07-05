eaf79865e8f490c3336971a48c35e6c6e1bf5d0d
Arya Satya <aryasatya6969@outlook.co.id>
2021-07-05 19:10:03 +0700

    menambah file file yang dibutuhkan

--- TR_GRAFKOM_A_672019289.cpp
+++ TR_GRAFKOM_A_672019289.cpp
@@ -1,2 +1,389 @@
-# file project pertama
-# cek adam
+#include <iostream>
+
+#include <GL/freeglut.h>
+
+#include <time.h> 
+#include <cmath>
+
+#include "loadImage.h";
+#include "objectDrawer.h";
+#include "colorOption.h";
+#include "myConfiguration.h";
+
+using namespace std;
+
+myConfiguration config;
+myColor color;
+myObject obj;
+
+void reshape(int width, int height) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(50.0, width / height, 5.0, 1000.0);
+	glTranslatef(-25.0, -150.0, -500.0);
+	glScalef(0.15, 0.15, 0.15);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, config.width, config.height);
+}
+
+void timer(int) {
+	glutTimerFunc(1000 / 30, timer, 0);
+	// Car
+	{
+		for (int i = 0; i < 5; i++) {
+			carPositionX[i] -= carSpeed[i];
+			for (int j = 0; j < 5; j++) {
+				float range = carPositionX[i] - carPositionX[j];
+				if (carPositionZ[i] == carPositionZ[j]) {
+					if (abs(range) < 50 && i != j) {
+						if (carSpeed[i] == 0) {
+							carSpeed[i] = carSpeed[j];
+						}
+						else {
+							if (carPositionX[i] > -300) {
+								if (carPositionZ[i] == 400) {
+									carPositionZ[i] = 475;
+									break;
+								}
+								else {
+									carPositionZ[i] = 400;
+									break;
+								}
+							}
+							else {
+								carSpeed[i] = carSpeed[j];
+								break;
+							}
+						}
+					}
+					if (abs(range) < 40 && abs(range) > -40 && i != j) {
+						if (carSpeed[i] > 0.0) {
+							carSpeed[i] -= 0.002;
+						}
+						else {
+							break;
+						}
+					}
+				}
+			}
+
+
+			if (carPositionX[i] < -415 && carPositionX[i] > -470) {
+				carSpeed[i] = 0;
+			}
+
+			if (carPositionX[i] < -415 && carPositionX[i] > -470) {
+				carSpeed[i] = rand() % ((4 - 2) + 1) + 2;
+			}
+
+			if (carPositionX[i] < -960) {
+				carPositionX[i] = 380;
+				carSpeed[i] = rand() % ((4 - 2) + 1) + 2;
+				carColor[i] = rand() % ((8 - 1) + 1) + 1;
+			}
+
+		}
+	}
+
+	// Tree Animation
+	{
+
+		if (treeAnimation < 5 && !treeAnimationLeft) {
+			treeAnimation += 0.2;
+		}
+		else {
+			treeAnimationLeft = true;
+		}
+
+		if (treeAnimation > -5 && treeAnimationLeft) {
+			treeAnimation -= 0.2;
+		}
+		else {
+			treeAnimationLeft = false;
+		}
+	}
+
+	glutPostRedisplay();
+
+}
+
+void display() {
+	if (config.nightMode) {
+		glClearColor(0, 0, 0, 1.0);
+	}
+	else {
+		glClearColor(0.93, 0.86, 0.64, 1.0);
+	}
+
+	if (config.manualLighting) {
+		GLfloat position[] = { config.mouseX, config.mouseY, config.mouseZ, 1 };
+		glLightfv(GL_LIGHT0, GL_POSITION, position);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+	}
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	glEnable(GL_COLOR_MATERIAL);
+	obj.drawBuilding();
+
+	obj.myTree(170.0, 340);
+	obj.myTree(0.0, 340);
+	obj.myTree(-100.0, 340);
+	obj.myTree(-200.0, 340);
+	obj.myTree(-330.0, 340);
+	obj.myTree(300.0, 580);
+	obj.myTree(120.0, 575);
+	obj.myTree(-90.0, 550);
+	obj.myTree(-220.0, 540);
+	obj.myTree(-770.0, 550);
+	obj.myTree(-670.0, 530);
+	obj.myTree(-470.0, 530);
+
+	obj.street();
+	obj.billboard();
+
+	obj.carObject(carPositionX[0], carPositionZ[0], carColor[0]);
+	obj.carObject(carPositionX[1], carPositionZ[1], carColor[1]);
+	obj.carObject(carPositionX[2], carPositionZ[2], carColor[2]);
+	obj.carObject(carPositionX[3], carPositionZ[3], carColor[3]);
+	obj.carObject(carPositionX[4], carPositionZ[4], carColor[4]);
+
+
+	obj.drawBuildingBase();
+	glDisable(GL_COLOR_MATERIAL);
+
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+// Action Controller
+
+void keyFun(unsigned char key, int x, int y) {
+	switch (key) {
+		// Right - Left
+	case 'a':
+		glRotatef(-5.0, 0.0, 1.0, 0.0);
+		break;
+	case 'd':
+		glRotatef(5.0, 0.0, 1.0, 0.0);
+		break;
+
+		// Up - Down
+	case 'w':
+		glRotatef(-5.0, 1.0, 0.0, 0.0);
+		break;
+	case 's':
+		glRotatef(5.0, 1.0, 0.0, 0.0);
+		break;
+
+		// Rotate - Z
+	case 'q':
+		glRotatef(5.0, 0.0, 0.0, 1.0);
+		break;
+	case 'e':
+		glRotatef(-5.0, 0.0, 0.0, 1.0);
+		break;
+
+		// enable manual lighting
+	case 'r':
+		if (!config.manualLighting) {
+			config.manualLighting = true;
+			sunlightAutoMovement = false;
+			cout << "Manual Lighting enabled!" << endl;
+		}
+		else {
+			config.manualLighting = false;
+			GLfloat position[] = { 400.0f, 100.0f, 500.0f, 0.5 };
+			glLightfv(GL_LIGHT0, GL_POSITION, position);
+			cout << "Manual Lighting disabled!" << endl;
+		}
+		break;
+
+	case 'g':
+		if (!sunlightAutoMovement) {
+			sunlightAutoMovement = true;
+			cout << "Auto movement lighting enabled!" << endl;
+		}
+		else {
+			GLfloat position[] = { 400.0f, 100.0f, 500.0f, 0.5 };
+			glLightfv(GL_LIGHT0, GL_POSITION, position);
+			sunlightAutoMovement = false;
+			cout << "Auto movement lighting disabled!" << endl;
+		}
+		break;
+
+	case 'n':
+		if (config.nightMode) {
+			config.nightMode = false;
+		}
+		else {
+			config.nightMode = true;
+		}
+		break;
+
+	case 'f':
+		if (!config.fscreen) {
+			glutFullScreen();
+			config.fscreen = true;
+			cout << "Fullscreen enabled!" << endl;
+		}
+		else {
+			config.fscreen = false;
+			glutReshapeWindow(config.width, config.height);
+			glutPositionWindow(config.windowPositionX, config.windowPositionY);
+			cout << "Fullscreen disabled!" << endl;
+		}
+		break;
+	case 'p':
+		for (int i = 0; i < 5; i++) {
+			carSpeed[i] = 3;
+		}
+		break;
+		// moving z point
+	case 't':
+		config.mouseZ += 100.0f;
+		break;
+	case 'y':
+		config.mouseZ -= 100.0f;
+		break;
+		// Zoom In - Out
+	case '1':
+		glScalef(1.025, 1.025, 1.025);
+		break;
+	case '2':
+		glScalef(0.975, 0.975, 0.975);
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void keySpecialFun(int key, int x, int y) {
+	switch (key) {
+		// Left
+	case 100:
+		glTranslatef(-5.0, 0.0, 0.0);
+		break;
+		// Up
+	case 101:
+		glTranslatef(0.0, 5.0, 0.0);
+		break;
+		// Right
+	case 102:
+		glTranslatef(5.0, 0.0, 0.0);
+		break;
+		// Down
+	case 103:
+		glTranslatef(0.0, -5.0, 0.0);
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		config.mouseDown = true;
+		config.xdiff = (x + config.xrot);
+		config.ydiff = (-y + config.yrot);
+	}
+	else {
+		config.mouseDown = false;
+	}
+}
+
+void mouseMotion(int x, int y) {
+	if (config.mouseDown)
+	{
+		config.xrot = (y + config.ydiff) / 150;
+		config.yrot = (x - config.xdiff) / 150;
+
+		glutPostRedisplay();
+
+		glRotatef(config.xrot, 1.0f, 0.0f, 0.0f);
+		glRotatef(config.yrot, 0.0f, 1.0f, 0.0f);
+	}
+
+
+}
+
+void mouseWheel(int button, int dir, int x, int y) {
+
+	if (dir > 0) {
+		glScalef(1.025, 1.025, 1.025);
+	}
+	else {
+		glScalef(0.975, 0.975, 0.975);
+	}
+	glutPostRedisplay();
+
+}
+
+void mouseMotionActive(int x, int y) {
+	config.mouseX = x;
+	config.mouseY = y;
+
+	glutPostRedisplay();
+}
+
+// Inisialisasi
+
+void myinit() {
+	glPointSize(10.0);
+	glLineWidth(2.5);
+	glMatrixMode(GL_PROJECTION);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LINE_SMOOTH);
+	glEnable(GL_POINT_SMOOTH);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glEnable(GL_NORMALIZE);
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, config.ambientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, config.diffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, config.specularLight);
+	glLightfv(GL_LIGHT0, GL_EMISSION, config.emission);
+	glLightfv(GL_LIGHT0, GL_POSITION, config.position);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	_textureID = loadBMPImage("./university-of-lausanne.bmp");
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(config.width, config.height);
+	glutInitWindowPosition(config.windowPositionX, config.windowPositionY);
+	glutCreateWindow("TR - Grafkom Asdos");
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyFun);
+	glutSpecialFunc(keySpecialFun);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+	glutTimerFunc(0, timer, 0);
+	glutMouseWheelFunc(mouseWheel);
+	glutPassiveMotionFunc(mouseMotionActive);
+
+	cout << " << Hint <<" << endl << endl;
+	cout << " > Press W, A, S, D to rotate the object" << endl;
+	cout << " > Press arrow button to translate the object" << endl;
+	cout << " > Press 1 or scroll up your mouse to increase scale" << endl;
+	cout << " > Press 2 or scroll up your mouse to decrease scale" << endl;
+	cout << " > Press r to enable/disable manual lighting" << endl;
+	cout << " > Press g to enable/disable auto movement lighting" << endl;
+	cout << " > Press t/y to adjust the depth of lighting" << endl;
+	cout << " > Press left click and hold your mouse to rotate the object" << endl;
+	cout << " > Press f to toggle fullscreen" << endl << endl;
+
+	myinit();
+	glutMainLoop();
+
+	return 0;
+}
---
+++ objectDrawer.h
@@ -0,0 +1,647 @@
+#pragma once
+
+#include "colorOption.h";
+#include "myConfiguration.h";
+
+#define deltat .001
+
+GLuint _textureID;
+
+float treeAnimation = 0;
+float treeAnimationLeft = true;
+
+bool sunlightAutoMovement = false;
+float sunlightPosition = 0;
+
+bool redOn = true;
+bool yellowOn = false;
+bool greenOn = false;
+
+float redLight = 1000, yellowLight = 0, greenLight = 0;
+
+float carSpeed[5] = { 3, 3, 3, 3, 3 };
+float carPositionX[5] = { 0, 300, 600, 600, 1200 };
+float carPositionZ[5] = { 400, 475, 400, 475, 400 };
+float carColor[5] = { 1 , 1 , 3, 4, 4 };
+float deltaCar[5] = { 0,0,0,0,0 };
+
+class myObject {
+
+public:
+	myColor color;
+	myConfiguration config;
+
+	void drawBuildingBase() {
+		glColor3ubv(color.groundAccent);
+		//Depan
+		glBegin(GL_POLYGON);
+		glVertex3f(-900.0, 0.0, 600.0);
+		glVertex3f(400.0, 0.0, 600.0);
+		glVertex3f(400.0, 50.0, 600.0);
+		glVertex3f(-900.0, 50.0, 600.0);
+		glEnd();
+
+		//belakang
+		glBegin(GL_POLYGON);
+		glVertex3f(-825.0, 0.0, -800.0);
+		glVertex3f(400.0, 0.0, -800.0);
+		glVertex3f(400.0, 50.0, -800.0);
+		glVertex3f(-825.0, 50.0, -800.0);
+		glEnd();
+
+		//kanan
+		glBegin(GL_POLYGON);
+		glVertex3f(400.0, 0.0, -800.0);
+		glVertex3f(400.0, 0.0, 600.0);
+		glVertex3f(400.0, 50.0, 600.0);
+		glVertex3f(400.0, 50.0, -800.0);
+		glEnd();
+
+		//kiri
+		glBegin(GL_POLYGON);
+		glVertex3f(-900.0, 0.0, 600.0);
+		glVertex3f(-1125.0, 0.0, -800.0);
+		glVertex3f(-1125.0, 50.0, -800.0);
+		glVertex3f(-900.0, 50.0, 600.0);
+		glEnd();
+
+		//bawah
+		glBegin(GL_POLYGON);
+		glVertex3f(-900.0, 0.0, 600.0);
+		glVertex3f(400.0, 0.0, 600.0);
+		glVertex3f(400.0, 0.0, -800.0);
+		glVertex3f(-1125.0, 0.0, -800.0);
+		glEnd();
+
+		//atas
+		glBegin(GL_POLYGON);
+		glColor3f(0.05, 0.63, 0);
+		glVertex3f(-900.0, 50.0, 600.0);
+		glVertex3f(400.0, 50.0, 600.0);
+		glVertex3f(400.0, 50.0, -800.0);
+		glVertex3f(-1125.0, 50.0, -800.0);
+		glEnd();
+
+	}
+
+
+
+	void drawBuilding() {
+
+		glColor3fv(color.lightGrey);
+
+		//bawah
+		glBegin(GL_POLYGON);
+		glVertex3f(-25.0, 55.0, -750.0);
+		glVertex3f(-500.0, 55.0, -750.0);
+		glVertex3f(-500.0, 55.0, -600.0);
+		glVertex3f(-25.0, 55.0, -600.0);
+		glEnd();
+
+		//depan
+		glBegin(GL_POLYGON);
+		glVertex3f(-500.0, 55.0, -400.0);
+		glVertex3f(-25.0, 55.0, -400.0);
+		glVertex3f(-25.0, 350.0, -400.0);
+		glVertex3f(-500.0, 350.0, -400.0);
+		glEnd();
+
+		//belakang
+		glBegin(GL_POLYGON);
+		glVertex3f(-25.0, 55.0, -750.0);
+		glVertex3f(-500.0, 55.0, -750.0);
+		glVertex3f(-500.0, 350.0, -750.0);
+		glVertex3f(-25.0, 350.0, -750.0);
+		glEnd();
+
+		//kanan
+		glBegin(GL_POLYGON);
+		glVertex3f(-25.0, 55.0, -750.0);
+		glVertex3f(-25.0, 55.0, -400.0);
+		glVertex3f(-25.0, 350.0, -400.0);
+		glVertex3f(-25.0, 350.0, -750.0);
+		glEnd();
+
+		//kiri
+		glBegin(GL_POLYGON);
+		glVertex3f(-500.0, 55.0, -750.0);
+		glVertex3f(-500.0, 55.0, -400.0);
+		glVertex3f(-500.0, 350.0, -400.0);
+		glVertex3f(-500.0, 350.0, -750.0);
+		glEnd();
+
+		glColor3f(0.9, 0.9, 0.9);
+		//atas
+		glBegin(GL_POLYGON);
+		glVertex3f(-25.0, 350.0, -750.0);
+		glVertex3f(-500.0, 350.0, -750.0);
+		glVertex3f(-500.0, 350.0, -400.0);
+		glVertex3f(-25.0, 350.0, -400.0);
+		glEnd();
+
+		//detail
+		glColor3fv(color.glass);
+		glBegin(GL_POLYGON);
+		glVertex3f(-450.0, 100.0, -399.5);
+		glVertex3f(-50.0, 100.0, -399.5);
+		glVertex3f(-50.0, 300.0, -399.5);
+		glVertex3f(-450.0, 300.0, -399.5);
+		glEnd();
+
+		glColor3fv(color.lightGrey);
+		float spacing = -400.0;
+		float spacingB = -350.0;
+		for (int i = 0; i < 5; i++) {
+			glBegin(GL_POLYGON);
+			glVertex3f(spacing, 75.0, -399.3);
+			glVertex3f(spacingB, 75.0, -399.3);
+			glVertex3f(spacingB, 350.0, -399.3);
+			glVertex3f(spacing, 350.0, -399.3);
+			glEnd();
+
+			spacing += 65.0;
+			spacingB += 65.0;
+		}
+
+		float spacingC = 300.0;
+		float spacingD = 325.0;
+		for (int i = 0; i < 3; i++) {
+			glBegin(GL_POLYGON);
+			glVertex3f(-500.0, spacingC, -399.1);
+			glVertex3f(-25.0, spacingC, -399.1);
+			glVertex3f(-25.0, spacingD, -399.1);
+			glVertex3f(-500.0, spacingD, -399.1);
+			glEnd();
+
+			spacingC -= 75.0;
+			spacingD -= 75.0;
+		}
+
+		glColor3fv(color.glass);
+		float spacingE = 275.0;
+		float spacingF = 300.0;
+		for (int i = 0; i < 5; i++) {
+			glBegin(GL_POLYGON);
+			glVertex3f(-500.5, spacingE, -700.0);
+			glVertex3f(-500.5, spacingE, -450.0);
+			glVertex3f(-500.5, spacingF, -450.0);
+			glVertex3f(-500.5, spacingF, -700.0);
+			glEnd();
+
+			glBegin(GL_POLYGON);
+			glVertex3f(-24.5, spacingE, -700.0);
+			glVertex3f(-24.5, spacingE, -450.0);
+			glVertex3f(-24.5, spacingF, -450.0);
+			glVertex3f(-24.5, spacingF, -700.0);
+			glEnd();
+
+			spacingE -= 50.0;
+			spacingF -= 50.0;
+		}
+
+		glColor3fv(color.darkGrey);
+		float spacingG = 310.0;
+		float spacingH = 300.0;
+		for (int i = 0; i < 5; i++) {
+			glBegin(GL_POLYGON);
+			glVertex3f(-375.0, spacingG, -750.5);
+			glVertex3f(-425.0, spacingG, -750.5);
+			glVertex3f(-425.0, spacingH, -750.5);
+			glVertex3f(-375.0, spacingH, -750.5);
+			glEnd();
+
+			glBegin(GL_POLYGON);
+			glVertex3f(-275.0, spacingG, -750.5);
+			glVertex3f(-325.0, spacingG, -750.5);
+			glVertex3f(-325.0, spacingH, -750.5);
+			glVertex3f(-275.0, spacingH, -750.5);
+			glEnd();
+
+			glBegin(GL_POLYGON);
+			glVertex3f(-175.0, spacingG, -750.5);
+			glVertex3f(-225.0, spacingG, -750.5);
+			glVertex3f(-225.0, spacingH, -750.5);
+			glVertex3f(-175.0, spacingH, -750.5);
+			glEnd();
+
+			glBegin(GL_POLYGON);
+			glVertex3f(-75.0, spacingG, -750.5);
+			glVertex3f(-125.0, spacingG, -750.5);
+			glVertex3f(-125.0, spacingH, -750.5);
+			glVertex3f(-75.0, spacingH, -750.5);
+			glEnd();
+
+			spacingG -= 50.0;
+			spacingH -= 50.0;
+		}
+
+		//detail atas
+		glColor3fv(color.grey);
+		glBegin(GL_POLYGON);
+		glVertex3f(-400.0, 350.5, -700.0);
+		glVertex3f(-450.0, 350.5, -700.0);
+		glVertex3f(-450.0, 350.5, -450.0);
+		glVertex3f(-400.0, 350.5, -450.0);
+		glEnd();
+		glColor3fv(color.lightGrey);
+		//belakang
+		glBegin(GL_POLYGON);
+		glVertex3f(-400.0, 350.5, -700.0);
+		glVertex3f(-450.0, 350.5, -700.0);
+		glVertex3f(-450.0, 360.5, -700.0);
+		glVertex3f(-400.0, 360.5, -700.0);
+		glEnd();
+		//depan
+		glBegin(GL_POLYGON);
+		glVertex3f(-450.0, 350.5, -450.0);
+		glVertex3f(-400.0, 350.5, -450.0);
+		glVertex3f(-400.0, 360.5, -450.0);
+		glVertex3f(-450.0, 360.5, -450.0);
+		glEnd();
+		//kiri
+		glBegin(GL_POLYGON);
+		glVertex3f(-450.0, 350.5, -700.0);
+		glVertex3f(-450.0, 350.5, -450.0);
+		glVertex3f(-450.0, 360.5, -450.0);
+		glVertex3f(-450.0, 360.5, -700.0);
+		glEnd();
+		//kanan
+		glBegin(GL_POLYGON);
+		glVertex3f(-400.0, 350.5, -700.0);
+		glVertex3f(-400.0, 350.5, -450.0);
+		glVertex3f(-400.0, 360.5, -450.0);
+		glVertex3f(-400.0, 360.5, -700.0);
+		glEnd();
+		glBegin(GL_POLYGON);
+		glVertex3f(-400.0, 360.5, -700.0);
+		glVertex3f(-450.0, 360.5, -700.0);
+		glVertex3f(-450.0, 360.5, -450.0);
+		glVertex3f(-400.0, 360.5, -450.0);
+		glEnd();
+
+		glColor3fv(color.grey);
+		glBegin(GL_POLYGON);
+		glVertex3f(-300.0, 350.5, -550.0);
+		glVertex3f(-350.0, 350.5, -550.0);
+		glVertex3f(-350.0, 350.5, -450.0);
+		glVertex3f(-300.0, 350.5, -450.0);
+		glEnd();
+		glColor3fv(color.lightGrey);
+		//belakang
+		glBegin(GL_POLYGON);
+		glVertex3f(-300.0, 350.5, -550.0);
+		glVertex3f(-350.0, 350.5, -550.0);
+		glVertex3f(-350.0, 360.5, -550.0);
+		glVertex3f(-300.0, 360.5, -550.0);
+		glEnd();
+		//depan
+		glBegin(GL_POLYGON);
+		glVertex3f(-350.0, 350.5, -450.0);
+		glVertex3f(-300.0, 350.5, -450.0);
+		glVertex3f(-300.0, 360.5, -450.0);
+		glVertex3f(-350.0, 360.5, -450.0);
+		glEnd();
+		//kiri
+		glBegin(GL_POLYGON);
+		glVertex3f(-350.0, 350.5, -550.0);
+		glVertex3f(-350.0, 350.5, -450.0);
+		glVertex3f(-350.0, 360.5, -450.0);
+		glVertex3f(-350.0, 360.5, -550.0);
+		glEnd();
+		//kanan
+		glBegin(GL_POLYGON);
+		glVertex3f(-300.0, 350.5, -550.0);
+		glVertex3f(-300.0, 350.5, -450.0);
+		glVertex3f(-300.0, 360.5, -450.0);
+		glVertex3f(-300.0, 360.5, -550.0);
+		glEnd();
+		glBegin(GL_POLYGON);
+		glVertex3f(-300.0, 360.5, -550.0);
+		glVertex3f(-350.0, 360.5, -550.0);
+		glVertex3f(-350.0, 360.5, -450.0);
+		glVertex3f(-300.0, 360.5, -450.0);
+		glEnd();
+
+	}
+
+
+
+	void myTree(float startPoint, float zPoint) {
+		// Batang
+		glColor3ub(139, 69, 19);
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint, 55, zPoint);
+		glVertex3f(startPoint - 10, 55, zPoint);
+		glVertex3f(startPoint - 10, 120, zPoint);
+		glVertex3f(startPoint, 120, zPoint);
+		glEnd();
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint, 55, zPoint - 10);
+		glVertex3f(startPoint - 10, 55, zPoint - 10);
+		glVertex3f(startPoint - 10, 120, zPoint - 10);
+		glVertex3f(startPoint, 120, zPoint - 10);
+		glEnd();
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint - 10, 55, zPoint - 10);
+		glVertex3f(startPoint - 10, 55, zPoint);
+		glVertex3f(startPoint - 10, 120, zPoint);
+		glVertex3f(startPoint - 10, 120, zPoint - 10);
+		glEnd();
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint, 55, zPoint - 10);
+		glVertex3f(startPoint, 55, zPoint);
+		glVertex3f(startPoint, 120, zPoint);
+		glVertex3f(startPoint, 120, zPoint - 10);
+		glEnd();
+
+		// daun
+		glColor3ub(19, 51, 17);
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint - 20, 90, zPoint + 40);
+		glVertex3f(startPoint + 20, 90, zPoint + 40);
+		glVertex3f(startPoint + 20, 90, zPoint - 40);
+		glVertex3f(startPoint - 20, 90, zPoint - 40);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint - 20 + treeAnimation, 180, zPoint + 40);
+		glVertex3f(startPoint + 20 + treeAnimation, 180, zPoint + 40);
+		glVertex3f(startPoint + 20 + treeAnimation, 180, zPoint - 40);
+		glVertex3f(startPoint - 20 + treeAnimation, 180, zPoint - 40);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glColor3ub(27, 78, 24);
+		glVertex3f(startPoint - 20, 90, zPoint + 40);
+		glColor3ub(19, 51, 17);
+		glVertex3f(startPoint - 20 + treeAnimation, 180, zPoint + 40);
+		glVertex3f(startPoint - 20 + treeAnimation, 180, zPoint - 40);
+		glVertex3f(startPoint - 20, 90, zPoint - 40);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint + 20, 90, zPoint + 40);
+		glVertex3f(startPoint + 20 + treeAnimation, 180, zPoint + 40);
+		glVertex3f(startPoint + 20 + treeAnimation, 180, zPoint - 40);
+		glVertex3f(startPoint + 20, 90, zPoint - 40);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glVertex3f(startPoint - 20, 90, zPoint - 40);
+		glVertex3f(startPoint + 20, 90, zPoint - 40);
+		glVertex3f(startPoint + 20 + treeAnimation, 180, zPoint - 40);
+		glVertex3f(startPoint - 20 + treeAnimation, 180, zPoint - 40);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glColor3ub(27, 78, 24);
+		glVertex3f(startPoint - 20, 90, zPoint + 40);
+		glColor3ub(19, 51, 17);
+		glVertex3f(startPoint + 20, 90, zPoint + 40);
+		glVertex3f(startPoint + 20 + treeAnimation, 180, zPoint + 40);
+		glVertex3f(startPoint - 20 + treeAnimation, 180, zPoint + 40);
+		glEnd();
+	}
+
+	void street() {
+
+		glBegin(GL_QUADS);
+		glColor3ubv(color.lightstreet);
+		glVertex3f(-930.0, 55.0, 360.0);
+		glColor3ubv(color.street);
+		glVertex3f(400.0, 55.0, 400.0);
+		glColor3ubv(color.street);
+		glVertex3f(400.0, 55.0, 520.0);
+		glColor3ubv(color.lightstreet);
+		glVertex3f(-920.0, 55.0, 480.0);
+		glEnd();
+
+		float width = 35.0f;
+		float margin = 80.0f;
+		float deltaZ = 0.0f;
+		float startPoint = -800.0f;
+		for (int i = 0; i < 16; i++) {
+			glBegin(GL_QUADS);
+			glColor3fv(color.white);
+			glVertex3f(startPoint, 56.0, 410.0 + deltaZ);
+			glVertex3f(startPoint - width, 56.0, 410.0 + deltaZ);
+			glVertex3f(startPoint - width, 56.0, 420.0 + deltaZ);
+			glVertex3f(startPoint, 56.0, 420.0 + deltaZ);
+			glEnd();
+
+			startPoint += margin;
+			deltaZ += 2.75;
+		}
+	}
+
+
+
+	void carObject(float xCoord, float zCoord, int colorOption) {
+
+		//bawah
+		glColor3fv(color.glass);
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord, 55.5, zCoord);
+		glVertex3f(xCoord, 55.5, zCoord + 25.0);
+		glVertex3f(xCoord + 35.0, 55.5, zCoord + 25.0);
+		glVertex3f(xCoord + 35.0, 55.5, zCoord);
+		glEnd();
+
+		//depan-mobil
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord, 55.5, zCoord);
+		glVertex3f(xCoord, 55.5, zCoord + 25.0);
+		glVertex3f(xCoord, 65.5, zCoord + 25.0);
+		glVertex3f(xCoord, 65.5, zCoord);
+		glEnd();
+
+		//blkg-mobil
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 35.0, 55.5, zCoord + 25.0);
+		glVertex3f(xCoord + 35.0, 55.5, zCoord);
+		glVertex3f(xCoord + 35.0, 65.5, zCoord);
+		glVertex3f(xCoord + 35.0, 65.5, zCoord + 25.0);
+		glEnd();
+
+		if (colorOption == 1)
+			glColor3fv(color.orange);
+
+		if (colorOption == 2)
+			glColor3fv(color.white);
+
+		if (colorOption == 3)
+			glColor3fv(color.darkGrey);
+
+		if (colorOption == 4)
+			glColor3ubv(color.darkBlue);
+
+		if (colorOption == 5)
+			glColor3fv(color.grey);
+
+		if (colorOption == 6)
+			glColor3fv(color.glass);
+
+		if (colorOption == 7)
+			glColor3fv(color.yellow);
+
+		if (colorOption == 8)
+			glColor3fv(color.red);
+
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord, 65.5, zCoord + 25.0);
+		glVertex3f(xCoord, 65.5, zCoord);
+		glVertex3f(xCoord + 35.0, 65.5, zCoord);
+		glVertex3f(xCoord + 35.0, 65.5, zCoord + 25.0);
+		glEnd();
+
+		//samping-mobil
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord, 65.5, zCoord);
+		glVertex3f(xCoord + 35.0, 65.5, zCoord);
+		glVertex3f(xCoord + 35.0, 55.5, zCoord);
+		glVertex3f(xCoord, 55.5, zCoord);
+		glEnd();
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord, 65.5, zCoord + 25.0);
+		glVertex3f(xCoord + 35.0, 65.5, zCoord + 25.0);
+		glVertex3f(xCoord + 35.0, 55.5, zCoord + 25.0);
+		glVertex3f(xCoord, 55.5, zCoord + 25.0);
+		glEnd();
+
+		//kaca
+		glColor3fv(color.white);
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 7.5, 65.7, zCoord);
+		glVertex3f(xCoord + 7.5, 65.7, zCoord + 25.0);
+		glVertex3f(xCoord + 27.5, 65.7, zCoord + 25.0);
+		glVertex3f(xCoord + 27.5, 65.7, zCoord);
+		glEnd();
+		glColor3fv(color.glass);
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 7.5, 65.7, zCoord);
+		glVertex3f(xCoord + 7.5, 65.7, zCoord + 25.0);
+		glVertex3f(xCoord + 10.0, 75.7, zCoord + 25.0);
+		glVertex3f(xCoord + 10.0, 75.7, zCoord);
+		glEnd();
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 27.5, 65.7, zCoord);
+		glVertex3f(xCoord + 27.5, 65.7, zCoord + 25.0);
+		glVertex3f(xCoord + 25.0, 75.7, zCoord + 25.0);
+		glVertex3f(xCoord + 25.0, 75.7, zCoord);
+		glEnd();
+		glColor3fv(color.white);
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 10.0, 75.7, zCoord);
+		glVertex3f(xCoord + 10.0, 75.7, zCoord + 25.0);
+		glVertex3f(xCoord + 25.0, 75.7, zCoord + 25.0);
+		glVertex3f(xCoord + 25.0, 75.7, zCoord);
+		glEnd();
+		glColor3fv(color.glass);
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 7.5, 65.7, zCoord);
+		glVertex3f(xCoord + 10.0, 75.7, zCoord);
+		glVertex3f(xCoord + 25.0, 75.7, zCoord);
+		glVertex3f(xCoord + 27.5, 65.7, zCoord);
+		glEnd();
+		glBegin(GL_POLYGON);
+		glVertex3f(xCoord + 7.5, 65.7, zCoord + 25.0);
+		glVertex3f(xCoord + 10.0, 75.7, zCoord + 25.0);
+		glVertex3f(xCoord + 25.0, 75.7, zCoord + 25.0);
+		glVertex3f(xCoord + 27.5, 65.7, zCoord + 25.0);
+		glEnd();
+	}
+
+
+
+	void billboard() {
+
+		glBegin(GL_QUADS);
+		glColor3ubv(color.hardGrey);
+		glVertex3f(-618.0, 55.0, 550.0);
+		glVertex3f(-613.0, 55.0, 550.0);
+		glVertex3f(-613.0, 105.0, 550.0);
+		glVertex3f(-618.0, 105.0, 550.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glVertex3f(-618.0, 55.0, 555.0);
+		glVertex3f(-613.0, 55.0, 555.0);
+		glVertex3f(-613.0, 105.0, 555.0);
+		glVertex3f(-618.0, 105.0, 555.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glVertex3f(-618.0, 55.0, 555.0);
+		glVertex3f(-618.0, 55.0, 550.0);
+		glVertex3f(-618.0, 105.0, 550.0);
+		glVertex3f(-618.0, 105.0, 555.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glVertex3f(-613.0, 55.0, 555.0);
+		glVertex3f(-613.0, 55.0, 550.0);
+		glVertex3f(-613.0, 105.0, 550.0);
+		glVertex3f(-613.0, 105.0, 555.0);
+		glEnd();
+
+		// Papan
+
+		glBegin(GL_QUADS);
+		glColor3ubv(color.hardGrey);
+		glVertex3f(-635.5, 105.5, 500.0);
+		glVertex3f(-630.5, 105.5, 500.0);
+		glVertex3f(-630.5, 200.0, 500.0);
+		glVertex3f(-635.5, 200.0, 500.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glColor3ubv(color.hardGrey);
+		glVertex3f(-595.5, 105.5, 605.0);
+		glVertex3f(-600.5, 105.5, 605.0);
+		glVertex3f(-600.5, 200.0, 605.0);
+		glVertex3f(-595.5, 200.0, 605.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glColor3ubv(color.hardGrey);
+		glVertex3f(-595.5, 200.5, 605.0);
+		glVertex3f(-630.5, 200.5, 500.0);
+		glVertex3f(-635.5, 200.5, 500.0);
+		glVertex3f(-600.5, 200.5, 605.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glColor3ubv(color.hardGrey);
+		glVertex3f(-595.5, 105.5, 605.0);
+		glVertex3f(-630.5, 105.5, 500.0);
+		glVertex3f(-635.5, 105.5, 500.0);
+		glVertex3f(-600.5, 105.5, 605.0);
+		glEnd();
+
+		glBegin(GL_QUADS);
+		glColor3fv(color.white);
+		glVertex3f(-635.5, 105.5, 500.0);
+		glVertex3f(-600.5, 105.5, 605.0);
+		glVertex3f(-600.5, 200.0, 605.0);
+		glVertex3f(-635.5, 200.0, 500.0);
+		glEnd();
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, _textureID);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glBegin(GL_QUADS);
+		glTexCoord2f(1, 0);
+		glVertex3f(-630.5, 105.5, 500.0);
+		glTexCoord2f(0, 0);
+		glVertex3f(-595.5, 105.5, 605.0);
+		glTexCoord2f(0, 1);
+		glVertex3f(-595.5, 200.0, 605.0);
+		glTexCoord2f(1, 1);
+		glVertex3f(-630.5, 200.0, 500.0);
+		glEnd();
+
+		glDisable(GL_TEXTURE_2D);
+	}
+};
